82ef4a56dacec7829648cce50f95d38c9f296512
Paul Khusainov <khusainov@aboutecho.com>
2013-09-04 17:13:34 +0400

    Add test for harlson's proto encoder and decoder

--- src/harlson_proto.erl
+++ src/harlson_proto.erl
@@ -82,3 +82,159 @@ read_times(N, Binary, Fun) when N > 0 ->
     {[Ret | RestRet], RestRestBinary}.
 
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+encode_test_() ->
+    [?_assertEqual(<<"GOVL">>, encode(get_over_limit)),
+     ?_assertEqual(<<"STOP">>, encode(stop)),
+     ?_assertEqual(<<"UPME", 1:?int, 
+                     4:?short, "test", 
+                     6:?short, "search",
+                     3:?short, "std",
+                     100500:?int
+                   >>,
+                   encode({update_metrics, [#q_metric{key = "test",
+                                                      endpoint = search,
+                                                      level = std,
+                                                      count = 100500}]})),
+     ?_assertEqual(<<"UPME", 2:?int, 
+                     4:?short, "test",   % 1st
+                     6:?short, "search",
+                     3:?short, "std",
+                     100500:?int,
+                     7:?short, "testkey",   % 2nd
+                     6:?short, "search",
+                     7:?short, "default",
+                     10501:?int
+                   >>,
+                   encode({update_metrics, [#q_metric{key = "test",
+                                                      endpoint = search,
+                                                      level = std,
+                                                      count = 100500},
+                                            #q_metric{key = "testkey",
+                                                      endpoint = search,
+                                                      level = default,
+                                                      count = 10501}]})),
+     ?_assertEqual(<<"UPME", 4:?int, 
+                     4:?short, "test",      % 1st
+                     6:?short, "search",
+                     3:?short, "std",
+                     100500:?int,
+                     7:?short, "testkey",   % 2nd
+                     6:?short, "search",
+                     7:?short, "default",
+                     10501:?int,
+                     4:?short, "test",      % 3rd
+                     6:?short, "search",
+                     3:?short, "std",
+                     100500:?int,
+                     7:?short, "testkey",   % 4nd
+                     6:?short, "search",
+                     7:?short, "default",
+                     10501:?int
+                   >>,
+                   encode({update_metrics, [#q_metric{key = "test",
+                                                      endpoint = search,
+                                                      level = std,
+                                                      count = 100500},
+                                            #q_metric{key = "testkey",
+                                                      endpoint = search,
+                                                      level = default,
+                                                      count = 10501},
+                                            #q_metric{key = "test",
+                                                      endpoint = search,
+                                                      level = std,
+                                                      count = 100500},
+                                            #q_metric{key = "testkey",
+                                                      endpoint = search,
+                                                      level = default,
+                                                      count = 10501}
+                                           ]})),
+     ?_assertEqual(<<"UPLI", 1:?int,
+                     3:?short, "std",
+                     6:?short, "search",
+                     1000000:?int
+                   >>,
+                   encode({update_limits, [#q_limit{level = std,
+                                                    endpoint = search,
+                                                    limit = 1000000}
+                                          ]})),
+     ?_assertEqual(<<"UPLI", 4:?int,
+                     3:?short, "std",
+                     6:?short, "search",
+                     1000000:?int,
+                     7:?short, "minimum",
+                     6:?short, "search",
+                     1000:?int,
+                     4:?short, "cool",
+                     6:?short, "search",
+                     2000000:?int,
+                     5:?short, "worst",
+                     6:?short, "search",
+                     2:?int
+                   >>,
+                   encode({update_limits, [#q_limit{level = std,
+                                                    endpoint = search,
+                                                    limit = 1000000},
+                                           #q_limit{level = minimum,
+                                                    endpoint = search,
+                                                    limit = 1000},
+                                           #q_limit{level = cool,
+                                                    endpoint = search,
+                                                    limit = 2000000},
+                                           #q_limit{level = worst,
+                                                    endpoint = search,
+                                                    limit = 2}
+                                          ]}))
+    ].
+
+decode_test_() ->
+    [?_assertEqual({overlimit, [#r_overlimit{key = "test",
+                                             endpoint = search,
+                                             change = #overlimit_add{value = 100,
+                                                                     throttle = 0.9}}
+                               ]},
+                   decode(<<"ROVL", 1:?int,
+                            4:?short, "test",
+                            6:?short, "search",
+                            0:?short, 100:?int, 900:?int
+                          >>)),
+     ?_assertEqual({overlimit, [#r_overlimit{key = "test",
+                                             endpoint = search,
+                                             change = #overlimit_add{value = 100,
+                                                                     throttle = 1.0}},
+                                #r_overlimit{key = "testkey",
+                                             endpoint = search,
+                                             change = overlimit_removed},
+                                #r_overlimit{key = "devel",
+                                             endpoint = search,
+                                             change = #overlimit_add{value = 1000000,
+                                                                     throttle = 0.8}},
+                                #r_overlimit{key = "general",
+                                             endpoint = search,
+                                             change = #overlimit_add{value = 10000000,
+                                                                     throttle = 0.7}}
+
+                               ]},
+                   decode(<<"ROVL", 4:?int,
+                            4:?short, "test", %-
+                            6:?short, "search",
+                            0:?short, 100:?int, 1000:?int,
+
+                            7:?short, "testkey", %-
+                            6:?short, "search",
+                            1:?short,
+
+                            5:?short, "devel", %-
+                            6:?short, "search",
+                            0:?short, 1000000:?int, 800:?int,
+
+                            7:?short, "general", %-
+                            6:?short, "search",
+                            0:?short, 10000000:?int, 700:?int
+                          >>))
+    ].
+
+-endif.
+
