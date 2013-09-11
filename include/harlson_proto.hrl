a083de658ab5acf81f6486dd3ce8183fa57d0859
Paul Khusainov <khusainov@aboutecho.com>
2013-09-11 14:01:52 +0400

    Limit is a string, not an atom

--- include/harlson_proto.hrl
+++ include/harlson_proto.hrl
@@ -1,7 +1,7 @@
 
 -type appkey()   :: nonempty_string().
 -type endpoint() :: atom().
--type level()    :: atom().
+-type level()    :: nonempty_string().
 
 -record(q_metric, {
         key      :: {appkey(), endpoint()},
